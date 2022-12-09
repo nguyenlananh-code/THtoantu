8c08cb19c0057b98a67bae2f5c0f4edb4bbc41fa
lananhjp <nguyenlananhjp0905@gmail.com>
2022-12-09 12:48:24 +0700

    thực hành toán tử

---
+++ bai1.js
@@ -0,0 +1,7 @@
+
+intputWight = prompt("Enter the Width");
+intputHight = prompt("Enter the Height");
+let Width = parseInt(intputWight);
+let Height = parseInt(intputHight);
+let area = Width * Height;
+document.write('The arae is:'+ area);
